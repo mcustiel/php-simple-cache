a7f8ba2436ec34a22a678e6b0181a87d3e6df7a3
Mariano Custiel <mcustiel@gmail.com>
2014-11-16 18:47:11 -0200

    Added continuous integration tools support into composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
+
 $loader->add('Mcustiel\\', __DIR__ . '/../src');
 $loader->add('Unit\\SimpleCache\\', __DIR__ . '/unit/Mcustiel/SimpleCache');
 $loader->add('Functional\\SimpleCache\\', __DIR__ . '/functional/Mcustiel/SimpleCache');
